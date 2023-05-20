ed4b8fede6cd2f7a233ce2d2d2290d0e99b71767
raphaeldevl <coreydaves@hotmail.it>
2023-05-20 17:29:24 -0300

    code finished as the tutorial, there are some issues to solve, the queen piece doent make the diagonal movment, that is a problem within bishop movements moves's code. Once resolve bishop, have to  resolve queen's diagonal movements

--- app.js
+++ app.js
@@ -332,15 +332,15 @@ function revertIDs(){
 }
 
 function checkWin(){
-    const kings = Array.from(document.querySelector('#king'))
+    const kings = Array.from(document.querySelectorAll('#king'))
    
-    if(!kings.some(king => king.firstChild.classList.contains('white'))){
+    if (!kings.some(king => king.firstChild.classList.contains('white'))){
         infoDisplay.innerHTML = "Black player wins!"
         const allSquares = document.querySelectorAll('.square')
         allSquares.forEach(square => square.firstChild?.setAttribute('draggable', false))
     }
 
-    if(!kings.some(king => king.firstChild.classList.contains('black'))){
+    if (!kings.some(king => king.firstChild.classList.contains('black'))){
         infoDisplay.innerHTML = "White player wins!"
         const allSquares = document.querySelectorAll('.square')
         allSquares.forEach(square => square.firstChild?.setAttribute('draggable', false))
