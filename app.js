e46ea280dcd226735adc11063d605b3a0fbf9209
raphaeldevl <coreydaves@hotmail.it>
2023-05-19 22:14:20 -0300

    criado logica de arrasto das peças - video em 37:23

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const gameBoard = document.querySelector("#gameboard")
 const playerDisplay = document.querySelector("#player")
 const infoDisplay = document.querySelector("#info-display")
-
 const width = 8
+let playerGo = 'black'
 
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
@@ -20,6 +20,7 @@ function createBoard(){
         const square = document.createElement('div')
         square.classList.add('square')
         square.innerHTML = startPiece
+        square.firstChild?.setAttribute('draggable', true)
         square.setAttribute('square-id', i)
         const row = Math.floor(( 63 - i) / 8) + 1
         if( row % 2 === 0 ){
@@ -39,3 +40,36 @@ function createBoard(){
 }
 
 createBoard()
+
+const allSquares = document.querySelectorAll("#gameboard .square")
+
+allSquares.forEach( square => {
+    square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
+})
+
+let startPositionId 
+let draggedElement
+
+function dragStart(e) {
+    startPositionId = e.target.parentNode.getAttribute('square-id')
+    draggedElement = e.target
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragDrop(e) {
+    e.stopPropagation()
+    const taken = e.target.classList.contains('piece')
+//     e.target.parentNode.append(draggedElement)
+//     e.target.remove()
+//    e.target.append(draggedElement)
+}
+
+
+
+
+
